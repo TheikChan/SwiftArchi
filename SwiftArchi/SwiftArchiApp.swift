8f95df3489f1ce9232399fe5ce50743a9b67dc81
Theik Chan <theik.chan@wavemoney.com.mm>
2024-09-07 01:17:50 +0700

    Initial Commit

---
+++ SwiftArchi/SwiftArchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftArchiApp.swift
+//  SwiftArchi
+//
+//  Created by Theik Chan on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftArchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
